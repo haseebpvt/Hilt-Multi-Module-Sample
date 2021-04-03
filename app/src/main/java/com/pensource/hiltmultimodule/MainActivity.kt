e7b7e79c9e63afc72c14999a19833f2fa2bd798e
haseebcgt <abdul.haseeb@cgtindia.in>
2021-04-03 23:38:18 +0530

    Using dagger binding

--- app/src/main/java/com/pensource/hiltmultimodule/MainActivity.kt
+++ app/src/main/java/com/pensource/hiltmultimodule/MainActivity.kt
@@ -2,12 +2,21 @@ package com.pensource.hiltmultimodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.pensource.data.StringHelper
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var stringHelper: StringHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("custom_log", stringHelper.capitalize("my text"))
     }
 }
